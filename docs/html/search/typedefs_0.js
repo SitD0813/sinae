02bf7c961e61e9a12fc103d7688d846c58b11b73
SitD0813 <sitd0813@gmail.com>
2020-02-05 06:59:39 -0500

    first commit

--- docs/html/search/typedefs_0.js
+++ docs/html/search/typedefs_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sn_5fdflow_5ffn_98',['sn_dflow_fn',['../group__operator__group.html#ga1575a24a6e259cbe59051185674b9aa1',1,'sinae_core.h']]],
+  ['sn_5fdflow_5ffn_98',['sn_dflow_fn',['../group__operator__group.html#ga090112c2b9a3e2dccd52ea151cfea8a2',1,'sinae_core.h']]],
   ['sn_5fflow_5ffn_99',['sn_flow_fn',['../group__operator__group.html#ga1900d989573dd1d9cae283643c357fa6',1,'sinae_core.h']]],
   ['sn_5fop_5ftype_100',['sn_op_type',['../group__operator__group.html#ga6e3d890a6c022d2b236e46354fb55686',1,'sinae_core.h']]]
 ];
